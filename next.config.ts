e78454b985ff83ec78123617428d37be2789678d
ArcidesFerrao <cidesferrao@gmail.com>
2026-06-03 11:12:20 +0200

    force-dynamic

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,14 @@ const nextConfig: NextConfig = {
       { protocol: "https", hostname: "uploadthing.com" },
     ],
   },
+  serverExternalPackages: ["pg", "@prisma/client", "@prisma/adapter-pg"],
+  webpack: (config: any) => {
+    config.experiments = {
+      ...config.experiments,
+      asyncWebAssembly: true,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
